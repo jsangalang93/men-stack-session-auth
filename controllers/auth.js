86bd66763444c4d682a1c8f514aa8b67fed96608
jsangalang93 <jsangalang93@gmail.com>
2024-05-02 18:07:13 -0400

    user creation works

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,11 +11,19 @@ router.get("/sign-up", (req, res) => {
 
   router.post("/sign-up", async (req, res) => {
     res.send("Form submission accepted!");
+    // Check if the username is already taken
     const userInDatabase = await User.findOne({ username: req.body.username });
-if (userInDatabase) {
-  return res.send("Username already taken.");
-}
-if (req.body.password !== req.body.confirmPassword) {
-  return res.send("Password and Confirm Password must match");
-}
+    if (userInDatabase) {
+      return res.send("Username already taken.");
+    }
+    // Check if the password and confirm password match
+    if (req.body.password !== req.body.confirmPassword) {
+      return res.send("Password and Confirm Password must match");
+    }
+    // Hash the password
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+    // Create the user
+    const user = await User.create(req.body);
+    res.send(`Thanks for signing up ${user.username}`);
   });
