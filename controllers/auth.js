6a5e71f6975a6cde1a710736d58835a71cb30791
jsangalang93 <jsangalang93@gmail.com>
2024-05-02 17:47:40 -0400

    create a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
 module.exports = router;
 
 router.get("/sign-up", (req, res) => {
-    res.render("sign-up.ejs");
+    res.render("auth/sign-up.ejs");
+  });
+
+  router.post("/sign-up", async (req, res) => {
+    res.send("Form submission accepted!");
+    const userInDatabase = await User.findOne({ username: req.body.username });
+if (userInDatabase) {
+  return res.send("Username already taken.");
+}
+if (req.body.password !== req.body.confirmPassword) {
+  return res.send("Password and Confirm Password must match");
+}
   });
